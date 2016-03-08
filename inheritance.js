79275344e1a36b6f8d03749077e794831536c4c9
awatson32 <audrey.watson32@gmail.com>
2016-03-08 17:36:12 -0600

    Added button functions to add vehicles to body

--- inheritance.js
+++ inheritance.js
@@ -1,54 +1,81 @@
-var Vehicle = function() {
+$(document).ready(function() {
+    function addCar() {
+        var car = new Vehicle();
+        car.insert();
+        
+    }
     
-}
-
-Vehicle.prototype.insert = function() {
-    this.div = document.createElement('div');
-    this.div.className = 'vehicle';
-    document.body.appendChild(this.div);
-}
-
-Vehicle.prototype.move = function() {
-    var div = this.div;
-    div.aniamate('slow');
-}
-
-Vehicle.prototype.damage = function() {
+    function addCop() {
+        
+    }
     
-}
+    function addMotorcycle() {
+        
+    }
+    
+    function addTank() {
+        
+    }
 
-Vehicle.prototype.remove = function() {
-    this.location = null;
-}
 
-var Car = function() {
-    Vehicle.call(this);
-    // -should be a sub class of vehicle with all shared properties
-	// 	-considered totaled once has 2 damage points
-	// 	-only moves horizontally across the browser
-	// 	-has a reverse function to reverse the direction of the car
-}
-Car.prototype = Object.create(Vehicle.prototype);
-Car.prototype.constructor = Car;
 
-var Cop = function () {
-    Vehicle.call();
-    // -should be a sub class of Car with all shared properties
-	// 	-considered totaled once has 3 damage points
-	// 	-only moves vertically across the browser
-	// 	-has a siren function which allows vehicle to blink at a set interval
-}
+    var Vehicle = function() {
+        
+    }
+    
+    // Methods
 
-var Motorcycle = function() {
-    Vehicle.call();
-    // -should be a sub class of vehicle with all shared properties
-	// 	-considered totaled after hit once (one damage point)
-	// 	-move diagonally
-	// 	-should set the speed property so that it moves twice as fast as normal vehicles
-}
-var Tank = function() {
-    Vehicle.call();
-    // -should be a sub class of vehicle with all shared properties
-	// 	-considered totaled once has 10 damage points
-	// 	-should set the speed property so that it moves half as fast as normal vehicles
-}
+    Vehicle.prototype.insert = function() {
+        this.div = document.createElement('div');
+        this.div.className = 'vehicle';
+        document.body.appendChild(this.div);
+    }
+
+    Vehicle.prototype.move = function() {
+        var div = this.div;
+        div.aniamate('slow');
+    }
+
+    Vehicle.prototype.damage = function() {
+        
+    }
+
+    Vehicle.prototype.remove = function() {
+        this.location = null;
+    }
+
+    //Constructors 
+
+    function Car() {
+        Vehicle.call(this); 
+        // -should be a sub class of vehicle with all shared properties
+        // 	-considered totaled once has 2 damage points
+        // 	-only moves horizontally across the browser
+        // 	-has a reverse function to reverse the direction of the car
+    }
+    Car.prototype = Object.create(Vehicle.prototype);
+    Car.prototype.constructor = Car;
+
+    function Cop() {
+        Vehicle.call(this);
+        // -should be a sub class of Car with all shared properties
+        // 	-considered totaled once has 3 damage points
+        // 	-only moves vertically across the browser
+        // 	-has a siren function which allows vehicle to blink at a set interval
+    }
+
+    function Motorcycle() {
+        Vehicle.call(this);
+        // -should be a sub class of vehicle with all shared properties
+        // 	-considered totaled after hit once (one damage point)
+        // 	-move diagonally
+        // 	-should set the speed property so that it moves twice as fast as normal vehicles
+    }
+    function Tank() {
+        Vehicle.call(this);
+        // -should be a sub class of vehicle with all shared properties
+        // 	-considered totaled once has 10 damage points
+        // 	-should set the speed property so that it moves half as fast as normal vehicles
+    }
+
+})
